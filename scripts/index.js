eec986edc230c790ee17df6d4c3aca0b8d18e15f
Yuri Ovechkin <cross19881228@yandex.ru>
2022-11-23 01:00:50 +0300

    after review

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
+// Находим DOM-элементы
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-btn');
 const popupOpenButtonElement = document.querySelector('.profile__edit-btn');
 const formElement = popupElement.querySelector('.popup__content');
-const likeElement =  document.querySelector('.element__like')
-const nameInput = popupElement.querySelector('.popup__name');
-const jobInput = popupElement.querySelector('.popup__job');
+const nameInput = popupElement.querySelector('.popup__input_type_name');
+const jobInput = popupElement.querySelector('.popup__input_type_job');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 
 
-
+// Открываем попап
 const openPopup = function () {
-    popupElement.classList.add('popup__is-opened');
+    popupElement.classList.add('popup__opened');
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
 }
 
-
+// Закрываем попап
 const closePopup = function () {
-    popupElement.classList.remove('popup__is-opened');
+    popupElement.classList.remove('popup__opened');
 }
 
-
+// Сохранение введенных в попап данных
 function formSubmitHandler(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
@@ -29,11 +29,7 @@ function formSubmitHandler(evt) {
     closePopup();
 }
 
-const likeCard = function () {
-    likeElement.classList.toggle('element__like_active');
-}
-
+// Прикрепляем обработчик к форме
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
-likeElement.addEventListener('click', likeCard);
