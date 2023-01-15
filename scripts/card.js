7c74d5b9b21fda31ee7710c583f2878ca8a361da
Yuri Ovechkin <cross19881228@yandex.ru>
2023-01-15 18:48:22 +0300

    after 1st review

--- scripts/card.js
+++ scripts/card.js
@@ -1,21 +1,19 @@
 export class Card {
     constructor(data, itemTemplateSelector, handleOpenImage) {
         this._data = data;
-        this._itemTemplate = document.querySelector(itemTemplateSelector).content.querySelector('.card');
-        this._itemElement = this._itemTemplate.cloneNode(true);
+        this._createCard = document.querySelector(itemTemplateSelector).content.querySelector('.card');
+        this._itemElement = this._createCard.cloneNode(true);
         this._itemDeleteButton = this._itemElement.querySelector('.card__delete-button');
         this._itemLikeButton = this._itemElement.querySelector('.card__item-like-button');
         this._cardImage = this._itemElement.querySelector('.card__image');
         this._handleOpenImage = handleOpenImage;
     }
-   
     _getTemplate() {
         const createCard =
         document.querySelector(itemTemplateSelector).content.querySelector('.card');
 
         return createCard;
     }
-
     _handleLikeButtonClick = () => {
         this._itemLikeButton.classList.toggle('card__item-like-button_active');
     }
